c6ec24b1644da32663009c90ca477f8474fbfbaa
eddestlamd <amanda.eddestal@hotmail.com>
2022-03-21 15:24:51 +0100

    user class, simple version

--- src/main/java/com/backend/Entity/User.java
+++ src/main/java/com/backend/Entity/User.java
@@ -1,20 +1,28 @@
 package com.backend.Entity;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Entity
 public class User {
 
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-
-
-
+    private String username;
+    private String password;
+    private String email;
 
 }
