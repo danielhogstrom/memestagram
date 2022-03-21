9cd32d29fef37315d901d10a857c06f62f89296f
eddestlamd <amanda.eddestal@hotmail.com>
2022-03-21 16:29:14 +0100

    some annotations

--- src/main/java/com/backend/Controller/UserController.java
+++ src/main/java/com/backend/Controller/UserController.java
@@ -1,17 +1,32 @@
 package com.backend.Controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.backend.Entity.User;
+import com.backend.Repository.UserRepository;
+import com.backend.Service.MemestagramService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
+    @Autowired
+    MemestagramService service;
+    @Autowired
+    UserRepository repository;
+
 
 @CrossOrigin(origins = "http://localhost:3000")
 @GetMapping("/")
 public String test () {
     return "hello hello";
 }
+/*
 
+    @PostMapping("/adduser")
+    User addUserObj(@RequestBody User user){
+        System.out.println("Called");
 
+        return repository.addUser(service.createUser(user));
+    }
+
+ */
 }
