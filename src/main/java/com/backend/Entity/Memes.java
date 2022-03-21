62358bc1e9f31f8a4ff547aa2fa89048cb0ffaac
Samuel <samuel.selomon@awacademy.training>
2022-03-21 15:42:03 +0100

    Validation

--- src/main/java/com/backend/Entity/Memes.java
+++ src/main/java/com/backend/Entity/Memes.java
@@ -1,9 +1,30 @@
 package com.backend.Entity;
 
-import javax.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.*;
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Entity
 public class Memes {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    private String picURL;
+    private String description;
+    Integer likes;
+    String comments;
+
+
+
 
 
 }
