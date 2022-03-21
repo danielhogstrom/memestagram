75d3a53f69e45ae9dd7e6c5ce414b3f3d16647e6
eddestlamd <amanda.eddestal@hotmail.com>
2022-03-21 15:55:43 +0100

    jparepository imports for user repository

--- src/main/java/com/backend/Repository/UserRepository.java
+++ src/main/java/com/backend/Repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.backend.Repository;
 
-public interface UserRepository {
+import com.backend.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
 }
