19946555deb73e11c88b186a9aad4dbf03edc558
hlykdly <mayamoto@protonmail.com>
2022-03-28 15:53:39 +0200

    Tests updated

--- src/test/java/com/backend/BackendApplicationTests.java
+++ src/test/java/com/backend/BackendApplicationTests.java
@@ -2,11 +2,14 @@ package com.backend;
 
 import com.backend.meme.Meme;
 import com.backend.meme.MemesRepository;
+import com.backend.user.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -16,6 +19,7 @@ class BackendApplicationTests {
     @Autowired
     MemesRepository underTest;
 
+
     @Test
     public void createMeme() {
         Meme meme = new Meme(null, "www.abc.com", "good", 4L, null);
@@ -23,27 +27,36 @@ class BackendApplicationTests {
     }
 
     @Test
-    public void memeRepository() {
-        List<Meme> asd = underTest.findAll();
-        assertThat(asd.size()).isEqualTo(3);
+    public void createUser() {
+        Set<Meme> myMemes = new HashSet<>();
+        User user = new User(null, "User", "pw", "myemail@great.com", "asdgg", myMemes);
+        assertThat(user.getUsername()).isEqualTo("User");
+        assertThat(user.getEmail()).isEqualTo("myemail@great.com");
     }
 
     @Test
-    public void asd() {
-        underTest.save(new Meme(null,"aa", "hej", 9L, null));
-        underTest.save(new Meme(null,"aa", "hej", 9L, null));
-        List<Meme> asd = underTest.findAll();
-        assertThat(asd.size()).isEqualTo(5);
+    public void memeRepositoryLoadingExistingData() {
+        List<Meme> listOfMemes = underTest.findAll();
+        assertThat(listOfMemes.size()).isEqualTo(3);
     }
 
     @Test
-    public void deleteRepositoryAndAddNewMeme () {
+    public void memeRepositorySavingData() {
+        underTest.save(new Meme(null,"asd", "hej1", 9L, null));
+        underTest.save(new Meme(null,"asdasd", "hej2", 14L, null));
+        List<Meme> listOfMemes = underTest.findAll();
+        assertThat(listOfMemes.size()).isEqualTo(5);
+    }
+
+    @Test
+    public void deleteMemeRepositoryAndAddNewData () {
         underTest.deleteAll();
         underTest.save(new Meme(null,"www.test.se", "this is a description", 5L, null));
-        List<Meme> asd = underTest.findAll();
-        assertThat(asd.get(0).getId()).isEqualTo(4);
-        assertThat(asd.get(0).getPicurl()).isEqualTo("www.test.se");
-        assertThat(asd.get(0).getDescription()).isEqualTo("this is a description");
+        List<Meme> listOfMemes = underTest.findAll();
+        assertThat(listOfMemes.get(0).getPicurl()).isEqualTo("www.test.se");
+        assertThat(listOfMemes.get(0).getDescription()).isEqualTo("this is a description");
+        assertThat(listOfMemes.size()).isEqualTo(1);
     }
+
 
 }
