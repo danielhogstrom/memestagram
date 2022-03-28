bf36b6c85ee4a36f044235e2ce3c35235a40bb99
hlykdly <mayamoto@protonmail.com>
2022-03-28 13:22:33 +0200

    Tests

--- src/test/java/com/backend/BackendApplicationTests.java
+++ src/test/java/com/backend/BackendApplicationTests.java
@@ -1,13 +1,15 @@
 package com.backend;
 
+import com.backend.meme.Meme;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
+
 
 }
