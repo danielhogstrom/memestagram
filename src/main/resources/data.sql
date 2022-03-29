9a756fcc65754870c70bc358ac740b0773f0f2f5
Samuel <samuel.selomon@awacademy.training>
2022-03-29 16:54:25 +0200

    Memes and users

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,21 @@
 INSERT INTO USERTABLE (USERNAME, PASSWORD, EMAIL) VALUES ('Ante', 'password123', 'ante@mail.com') ;
 INSERT INTO USERTABLE (USERNAME, PASSWORD, EMAIL) VALUES ('Chris', 'password123', 'chris@mail.com');
 INSERT INTO USERTABLE (USERNAME, PASSWORD, EMAIL) VALUES ('Samuel', 'password123', 'samuel@mail.com');
+INSERT INTO USERTABLE (USERNAME, PASSWORD, EMAIL) VALUES ('Johan', 'password123', 'johan@mail.com');
+INSERT INTO USERTABLE (USERNAME, PASSWORD, EMAIL) VALUES ('Nicke', 'password123', 'nicke@mail.com');
+INSERT INTO USERTABLE (USERNAME, PASSWORD, EMAIL) VALUES ('Hanna', 'password123', 'hanna@mail.com');
+INSERT INTO USERTABLE (USERNAME, PASSWORD, EMAIL) VALUES ('Erika', 'password123', 'erika@mail.com');
+
 INSERT INTO MEME (PICURL, DESCRIPTION, LIKES, USER_ID) VALUES ('https://i.redd.it/uqk46zan8zo81.jpg','Finally someone who understands my humor!!', 12, 1);
 INSERT INTO MEME (PICURL, DESCRIPTION, LIKES, USER_ID) VALUES ('https://i.imgur.com/x51rrjw.jpeg','Thats a mental illness, right??',11, 2);
 INSERT INTO MEME (PICURL, DESCRIPTION, LIKES, USER_ID) VALUES ('https://i.redd.it/9iodhe6ss2p81.jpg','Who can relate?', 31, 3);
+INSERT INTO MEME (PICURL, DESCRIPTION, LIKES, USER_ID) VALUES ('https://rankedbyvotes.com/wp-content/uploads/When-I-try-to-Smile.gif','100 % success rate', 21, 3);
+INSERT INTO MEME (PICURL, DESCRIPTION, LIKES, USER_ID) VALUES ('https://pd-beamliving-cd.beamliving.com/-/media/bu-to-ch/cat-meme-netflix-funny-1000x666.png','Of course i am!', 11, 5);
+INSERT INTO MEME (PICURL, DESCRIPTION, LIKES, USER_ID) VALUES ('https://64.media.tumblr.com/8b33555001dfd1f4c2b4a0d8894a6839/a3c4c5555a725646-ba/s640x960/4e02167f7137973c21ae0a55a94ce51f1fe0fe61.jpg','Wise words', 44, 6);
+INSERT INTO MEME (PICURL, DESCRIPTION, LIKES, USER_ID) VALUES ('https://filmdaily.co/wp-content/uploads/2020/12/willsmith-2-.jpg','Hard times', 7,6 );
+INSERT INTO MEME (PICURL, DESCRIPTION, LIKES, USER_ID) VALUES ('https://res.cloudinary.com/purnesh/image/upload/w_1000,f_auto,q_auto:eco,c_limit/71586859071755.jpg','True', 7,4 );
+INSERT INTO MEME (PICURL, DESCRIPTION, LIKES, USER_ID) VALUES ('https://brobible.com/wp-content/uploads/2022/01/50-best-funny-memes-2022-so-far.jpg','It tastes like water', 7,6 );
+INSERT INTO MEME (PICURL, DESCRIPTION, LIKES, USER_ID) VALUES ('https://images3.memedroid.com/images/UPLOADED536/6172f953593dc.jpeg','Always!', 11,2 );
+INSERT INTO MEME (PICURL, DESCRIPTION, LIKES, USER_ID) VALUES ('https://www.jokejok.com/wp-content/uploads/2021/03/tenor.gif','Dramaqueen', 51, 4);
+INSERT INTO MEME (PICURL, DESCRIPTION, LIKES, USER_ID) VALUES ('https://pbs.twimg.com/media/DlYjEZkWsAAgEjG?format=jpg&name=small','When you work with React', 21, 1);
 
